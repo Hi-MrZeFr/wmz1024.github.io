af86ebf778ffca1f6650976799889a24a9742154
wmz1024 <i@wmza.cn>
2021-09-18 19:56:22 +0800

    hiw

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "5px 5px 5px 5px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "10",
+    "top": "10",
+    "key": "ca9bef677d5d490e89c216bc45fa6377"
+  }
+}
